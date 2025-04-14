5faa6ba3f5f6d3a3aa8b9b13c6254ff9441877ab
amara212 <96530980+AmaraX7@users.noreply.github.com>
2025-04-14 15:05:38 +0200

    Add files via upload

---
+++ html/variables_4.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['vender_161',['vender',['../classBarco.html#a3463631b4ebd3248975f051247f7201d',1,'Barco']]],
+  ['visitadas_162',['visitadas',['../classBarco.html#afdf042dd4585fa01f310db4b33e522c2',1,'Barco']]],
+  ['volumen_163',['volumen',['../classProducto.html#a6d13a8d1a5dbe354f421e14e88a08273',1,'Producto']]]
+];
